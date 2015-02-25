08444a52eebf0295b6b439317ca2f9c52068a9f1
pigram86 <todd@toddpigram.com>
2015-02-25 14:13:45 -0500

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,13 @@
+name             'win_tasks'
+maintainer       'Todd Pigram'
+maintainer_email 'todd@toddpigram.com'
+license          'Apache 2.0'
+description      'Installs/Configures win_tasks'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.2.0'
+depends          'windows'
+
+
+
+supports "windows"
+
